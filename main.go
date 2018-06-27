57ad4d439d87325387ba9d0c32efeefc1c27330e
MengZnLee <adnt587@gmail.com>
2018-06-27 14:57:33 +0800

    add main

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/openstack"
+	"fmt"
+)
+
+func main() {
+	authOpts := gophercloud.AuthOptions{
+		IdentityEndpoint: "http://10.0.0.21:35357/v3",
+		Username:         "admin",
+		Password:         "h2u1E1mKp3jFfFKriBE55bHTtdeZYKEMVcWf0Ron",
+		DomainName:       "Default",
+		TenantName:       "admin",
+	}
+	provider, err := openstack.AuthenticatedClient(authOpts)
+	if err != nil {
+		fmt.Println("Fatal AuthenticatedClient")
+		fmt.Errorf("Fatal error autenticating:  %s \n", err)
+	}
+	_, err = openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: "RegionOne",
+	})
+	if err != nil {
+		fmt.Println("Fatal NewComputeV2")
+		fmt.Errorf("Fatal error NewComputeV2:  %s \n", err)
+	}
+}
